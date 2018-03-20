aa96f7949345e55134e67bc8803d6cf795de9736
Anoop <anoop@whizz.com.au>
2018-03-21 10:00:46 +1100

    Cleared the Disposibles on viewModel onCreated

--- app/src/main/java/au/com/sentina/viewmodel/PropertyViewModel.kt
+++ app/src/main/java/au/com/sentina/viewmodel/PropertyViewModel.kt
@@ -6,36 +6,44 @@ import au.com.sentina.data.Data
 import au.com.sentina.data.Properties
 import au.com.sentina.repository.PropertyRepository
 import au.com.sentina.webservice.APICallBacks
+import io.reactivex.disposables.CompositeDisposable
 
 
-class PropertyViewModel: ViewModel() {
-    private var properties:MutableLiveData<Properties>?=null
-    private var selectedProperty:MutableLiveData<Data> =MutableLiveData()
-    private fun loadProperties(){
-        PropertyRepository(object :APICallBacks<Properties>{
-            override fun setError(message: String) {
-            }
-            override fun setResult(t: Properties) {
-                android.util.Log.e(PropertyViewModel::class.simpleName,t.toString())
-                properties?.value=t
-            }}).getProperties()
+class PropertyViewModel : ViewModel() {
+    private var properties: MutableLiveData<Properties>? = null
+    private var selectedProperty: MutableLiveData<Data> = MutableLiveData()
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    private var propertyRepository: PropertyRepository = PropertyRepository(object : APICallBacks<Properties> {
+        override fun setError(message: String) {
+        }
+
+        override fun setResult(t: Properties) {
+            properties?.value = t
+        }
+    })
+
+    private fun loadProperties() {
+        compositeDisposable.add(propertyRepository.getProperties())
     }
 
-    fun select(data: Data){
-        selectedProperty.value=data
+    fun select(data: Data) {
+        selectedProperty.value = data
     }
 
-    fun getProperties():MutableLiveData<Properties>?{
-        if(null==properties){
-            properties= MutableLiveData()
+    fun getProperties(): MutableLiveData<Properties>? {
+        if (null == properties) {
+            properties = MutableLiveData()
             loadProperties()
         }
         return properties
     }
 
-    fun getSelectedProperty(): MutableLiveData<Data>?{
+    fun getSelectedProperty(): MutableLiveData<Data>? {
         return selectedProperty
     }
 
+    override fun onCleared() {
+        compositeDisposable.clear()
+    }
 
 }
