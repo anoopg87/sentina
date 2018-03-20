cc652c0f90b836993cba01eab8a10032c2e3f6b0
Anoop <anoop@whizz.com.au>
2018-03-21 10:02:52 +1100

    Disposible implementation

--- app/src/main/java/au/com/sentina/repository/PropertyRepository.kt
+++ app/src/main/java/au/com/sentina/repository/PropertyRepository.kt
@@ -3,25 +3,23 @@ package au.com.sentina.repository
 import au.com.sentina.data.Properties
 import au.com.sentina.webservice.APICallBacks
 import au.com.sentina.webservice.APIImplementation
-import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 
 class PropertyRepository(private val callBack: APICallBacks<Properties>) {
 
-    fun  getProperties(){
-        val properties: Flowable<Properties>? = APIImplementation().getProperties()
-            properties
-                    ?.subscribeOn(Schedulers.io())
-                    ?.observeOn(AndroidSchedulers.mainThread())
-                    ?.subscribe(
-                            {
-                                callBack.setResult(it)
-                            },
-                            {
-                                callBack.setError(it.localizedMessage)
-                            })
-
+    fun getProperties(): Disposable? {
+        return APIImplementation().getProperties()
+                ?.subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribe(
+                        {
+                            callBack.setResult(it)
+                        },
+                        {
+                            callBack.setError(it.localizedMessage)
+                        })
     }
 }
